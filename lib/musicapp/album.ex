06dba9129a68f93265fd0e9cbfa64e4672a1d62a
Noelia Fernández Ena <noelia.ferena@gmail.com>
2019-01-16 23:34:51 +0100

    albums endpoint

--- lib/musicapp/album.ex
+++ lib/musicapp/album.ex
@@ -1,5 +1,6 @@
 defmodule Musicapp.Album do
     use Ecto.Schema
+    import Ecto.Changeset
 
     @derive {Jason.Encoder, except: [:__meta__, :__struct__]}
 
@@ -10,4 +11,9 @@ defmodule Musicapp.Album do
         belongs_to :artist, Musicapp.Artist
     end
 
+    def changeset(struct, params) do
+        struct
+            |> cast(params, [:title, :tracks, :artist_id])
+            |> validate_required([:title])
+    end
 end
--- lib/musicapp/endpoint.ex
+++ lib/musicapp/endpoint.ex
@@ -4,6 +4,7 @@ defmodule Musicapp.Endpoint do
 
   alias Musicapp.Repo
   alias Musicapp.Artist
+  alias Musicapp.Album
 
   plug(:match)
   plug(:dispatch)
@@ -13,7 +14,7 @@ defmodule Musicapp.Endpoint do
   get "/api/artists" do
     conn 
       |> put_resp_content_type("application/json")
-      |> send_resp(200, Jason.encode!(Musicapp.Repo.all(Musicapp.Artist)))
+      |> send_resp(200, Jason.encode!(Repo.all(Artist)))
   end
 
   get "/api/artists/:id" do
@@ -54,6 +55,38 @@ defmodule Musicapp.Endpoint do
     end
   end
 
+  get "/api/albums" do
+    conn 
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, Jason.encode!(Repo.preload(Repo.all(Album), :artist)))
+  end
+
+  get "/api/albums/:id" do
+    album = Repo.get(Album, conn.path_params["id"])
+    if is_nil(album) do
+      conn 
+        |> put_resp_content_type("application/json")
+        |> send_resp(404, missing_album(conn.path_params["id"]))
+    else
+      conn 
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, Jason.encode!(album))
+    end
+  end
+
+  post "/api/albums" do
+    {:ok, body, conn} = Plug.Conn.read_body(conn)
+    IO.inspect Jason.decode!(body)
+    
+    new_album = Album.changeset(%Album{}, Jason.decode!(body))
+      |> Repo.preload(:artist)
+      |> Repo.insert!()
+
+    conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(201, Jason.encode!(new_album))
+  end
+
   match _ do
       conn 
         |> put_resp_content_type("application/json")
@@ -68,5 +101,9 @@ defmodule Musicapp.Endpoint do
 
   defp missing_artist(id) do 
     Jason.encode!(%{:error => "The artist with id "<>id<>" does not exist"})
+  end
+
+  defp missing_album(id) do 
+    Jason.encode!(%{:error => "The album with id "<>id<>" does not exist"})
   end
 end
